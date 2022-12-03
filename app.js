25ee142413ab47957fecd6e92829bd82732ce1c5
JoaoBRBR <drvictor321@gmail.com>
2022-12-02 22:05:03 -0300

    quality of life changes

--- app.js
+++ app.js
@@ -24,3 +24,43 @@ function show() {
 function close() {
     mainMenu.style.top = '-100%';
 }
+
+//function valida() {
+//    if (document.getElementById('audioOn').checked) {
+//        document.getElementById('audioLigado').style.display = 'block';
+//        document.getElementById('audioDesligado').style.display = 'none';
+//        document.getElementById('videoBanner').setAttribute('muted',false)
+//    } else {
+//        document.getElementById('audioLigado').style.display = 'none';
+//        document.getElementById('audioDesligado').style.display = 'block';
+//        document.getElementById('videoBanner').removeAttribute("muted",true);
+//    }
+//}
+
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+const hiddenElements = document.querySelectorAll('.hiddenL');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const observerB = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+const hiddenElementsB = document.querySelectorAll('.hiddenR');
+hiddenElementsB.forEach((el) => observerB.observe(el));
