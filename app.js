729f82ba8447863c3079d1b353e259c33b02c0ad
JoaoBRBR <drvictor321@gmail.com>
2022-12-22 11:55:55 -0300

    Novas Imagens

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ var counter = 1;
 setInterval(function(){
     document.getElementById("radio"+counter).checked = true;
     counter++;
-    if(counter>4){
+    if(counter>6){
         counter = 1;
     }
 },3000);
